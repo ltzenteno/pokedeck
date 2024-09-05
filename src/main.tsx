15209357baabd700a6d08adae32cd94551988a77
Luis Zenteno <ltzenteno@gmail.com>
2024-09-05 10:37:07 -0600

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css'
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Home</div>
+    element: <h1 className="text-3xl font-bold underline">Home</h1>
   },
   {
     path: '/detail/:id',
-    element: <div>Detail</div>
-  }
+    element: <h1 className="text-3xl font-bold underline">Detail</h1>
+  },
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
